b0a3c5ea01c82c91d26899807db4d444ec39f006
Bruno <mct.bruno@gmail.com>
2025-04-20 07:36:24 +0200

    Add dashboard for EasyAdmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Recipe\Ingredient;
+use App\Entity\Recipe\Recipe;
+use App\Entity\Recipe\RecipeIngredient;
+use App\Entity\Recipe\RecipeStep;
+use App\Entity\Recipe\Utensil;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+#[AdminDashboard(routePath: '/admin', routeName: 'admin')]
+class DashboardController extends AbstractDashboardController
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private TranslatorInterface $translator,
+    ){
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $recipesCount = $this->entityManager->getRepository(Recipe::class)->count([]);
+
+         return $this->render('admin/dashboard.html.twig', [
+             'recipesCount' => $recipesCount,
+         ]);
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Meal Planner')
+            ;
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud($this->translator->trans('Recipe'), 'fa-solid fa-book', Recipe::class);
+        yield MenuItem::linkToCrud($this->translator->trans('Recipe steps'), 'fa-solid fa-list-ol', RecipeStep::class);
+        yield MenuItem::linkToCrud($this->translator->trans('Ingredients'), 'fa-solid fa-carrot', Ingredient::class);
+        yield MenuItem::linkToCrud($this->translator->trans('Ingredients - recipe'), 'fa-solid fa-burger', RecipeIngredient::class);
+        yield MenuItem::linkToCrud($this->translator->trans('Utensils'), 'fa-solid fa-utensils', Utensil::class);
+    }
+}
